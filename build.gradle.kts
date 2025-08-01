71eb79d219bcf7b2de23abdd4ac0de5b1d2c87bc
InvalidJoker <0limitdev@gmail.com>
2025-08-01 18:38:04 +0200

    feat: 1.21.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,5 @@ subprojects {
 
     // Set the group ID for all projects in the buildSrc module.
     group = "de.joker"
-    version = "1.21.5-${projectVersion}"
+    version = "1.21.6-${projectVersion}"
 }
